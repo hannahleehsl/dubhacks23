728471cc2376b86f0160611c0fb7cbc60492826f
hsl5 <hsl5@uw.edu>
2023-10-14 16:23:55 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import openai from 'openai';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const configuration = new openai.Configuration({
+  apiKey: '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',
+});
+
+const openaiClient = new openai.OpenAIApi(configuration);
+
+const app = express();
+const port = 3002;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/', async (req, res) => {
+
+  const {message} = req.body;
+  const completion = await openaiClient.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'user', content: message },
+      ],
+    })
+
+    res.json({
+      completion: completion.data.choices[0].message.content,
+    })
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
