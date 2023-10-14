3b15ab91261a14f15a22160b55ffce223e40403b
hsl5 <hsl5@uw.edu>
2023-10-14 16:25:16 -0700

    new apikey

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 const configuration = new openai.Configuration({
-  apiKey: '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',
+  apiKey: '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',
 });
 
 const openaiClient = new openai.OpenAIApi(configuration);
